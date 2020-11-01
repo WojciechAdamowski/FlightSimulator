23edaef9d59a8b980d725a50b37c723be9d0bc44
Wojtek <Adamowski>
2020-11-01 12:01:48 +0100

    Create exception for doesn't exist pilot status

---
+++ src/main/java/employmentDepartment/exceptions/PilotStatusDoesNotExist.java
@@ -0,0 +1,5 @@
+package employmentDepartment.exceptions;
+
+public class PilotStatusDoesNotExist  extends Exception{
+    public PilotStatusDoesNotExist(String errorMessage){ super(errorMessage); }
+}
