8c0a0ea4b9f9f9c3404eb04c2daaffa748928569
Wojtek <Adamowski>
2020-11-05 12:42:06 +0100

    Add new exceptions for input PilotPhone

---
+++ src/main/java/employmentDepartment/exceptions/pilotPhoneNumber/PilotPhoneDoesNotHaveNineNumbers.java
@@ -0,0 +1,5 @@
+package employmentDepartment.exceptions.pilotPhoneNumber;
+
+public class PilotPhoneDoesNotHaveNineNumbers extends Exception{
+    PilotPhoneDoesNotHaveNineNumbers (String errorMessage){ super(errorMessage); }
+}
---
+++ src/main/java/employmentDepartment/exceptions/pilotPhoneNumber/PilotPhoneIsNotANumber.java
@@ -0,0 +1,5 @@
+package employmentDepartment.exceptions.pilotPhoneNumber;
+
+public class PilotPhoneIsNotANumber extends Exception{
+    PilotPhoneIsNotANumber( String errorMessage ){ super(errorMessage); }
+}
