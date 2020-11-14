71ecb7bc5c633ef31f302cbe6bbc79d128805bde
Wojtek <Adamowski>
2020-11-14 16:49:46 +0100

    Add new exception for pilot's salary

---
+++ src/main/java/employmentDepartment/exceptions/PilotSalary/PilotSalaryIsNotANumberException.java
@@ -0,0 +1,5 @@
+package employmentDepartment.exceptions.PilotSalary;
+
+public class PilotSalaryIsNotANumberException extends Exception{
+    public PilotSalaryIsNotANumberException(String errorMessage){super(errorMessage);}
+}
