db622646b72884b0325d248e1d62f07b40c8640b
Wojtek <Adamowski>
2020-11-14 17:26:20 +0100

    Delete unnecessary code

--- tests/employmentDepartment/EmploymentDepartmentTest.java
+++ tests/employmentDepartment/EmploymentDepartmentTest.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EmploymentDepartmentTest {
 
-    EmploymentDepartment employmentDepartment = EmploymentDepartment.getInstance();
-
     @Test
     void checkReturnWithCorrectDataCheckCorrectnessAndGetPilotStatus(){
     }
